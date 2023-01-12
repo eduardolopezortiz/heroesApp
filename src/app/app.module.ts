e28ffcfb48cd42abf330877462dd64c638faf8ba
Brandon Eduardo López Ortiz <eduardolopezortiz95@gmail.com>
2023-01-12 01:00:48 -0600

    Modulos y componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgregarComponent } from './heroes/pages/agregar/agregar.component';
+import { BuscarComponent } from './heroes/pages/buscar/buscar.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent,
+    BuscarComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule
