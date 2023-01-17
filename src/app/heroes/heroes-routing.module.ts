ff81c9494fad11f79397742e8627f0608b920ece
Brandon Eduardo López Ortiz <eduardolopezortiz95@gmail.com>
2023-01-16 23:36:59 -0600

    Mostrar rutas hijas, segundo RouterOutLet

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 
 const rutas: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
 
       { path: 'listado', component: ListadoComponent },
